8948afb04939c348c7eb839a71314d93f6d6ca6e
santiago diaz <sidiaz4@miuandes.cl>
2021-05-02 21:51:38 -0400

    gitignore

---
+++ fib.js
@@ -0,0 +1,22 @@
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+ 
+function fib(n) {
+    if (n <= 1){
+        return n
+    }
+    else{
+        cosito = fib(n-1)+fib(n-2)
+        return cosito
+    }
+}
+  
+readline.question("A number pls: ", n => {
+    var m = fib(n);
+    console.log("The result is: fib =", + m);
+    readline.close()
+});
+
